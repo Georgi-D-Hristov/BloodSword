30b862428ac67e432496155dca44da0300658133
Georgi Hristov <georgi.dh@gmail.com>
2025-11-10 15:29:05 +0200

    feat(domain): add hero, item and inventory entities

---
+++ BloodSword.Domain/Entities/InventoryItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodSword.Domain.Entities
+{
+    internal class InventoryItem
+    {
+    }
+}
---
+++ BloodSword.Domain/Enums/ItemType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodSword.Domain.Enums
+{
+    internal class ItemType
+    {
+    }
+}
