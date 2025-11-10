e438a1d8025f2b1bbba0768a153f93d292b290db
Georgi Hristov <georgi.dh@gmail.com>
2025-11-10 16:44:26 +0200

    feat(api): configure db context, identity and user secrets

--- BloodSword.WebAPI/Program.cs
+++ BloodSword.WebAPI/Program.cs
@@ -1,6 +1,35 @@
+﻿using BloodSword.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// 1. Взимаме connection string-а
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// 2. Регистрираме DbContext-а с SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString,
+        // Тази настройка казва на EF Core, че миграциите се намират в WebAPI проекта,
+        // въпреки че самият DbContext е в Infrastructure.
+        b => b.MigrationsAssembly("BloodSword.WebAPI")));
+
+// 3. Регистрираме Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    // Тук можем да настроим правилата за пароли (по желание)
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddControllers();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
