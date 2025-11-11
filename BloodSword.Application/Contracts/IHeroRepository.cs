e97cdcbab617ce500e7e18fa9f03541840e75392
Georgi Hristov <Georgi.dh@gmail.com>
2025-11-12 00:50:24 +0200

    feat(app): define IHeroRepository contract

---
+++ BloodSword.Application/Contracts/IHeroRepository.cs
@@ -0,0 +1,15 @@
+﻿using BloodSword.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace BloodSword.Application.Contracts
+{
+    public interface IHeroRepository
+    {
+        Task<Hero> GetByIdAsync(Guid id);
+        Task<Hero> CreateAsync(Hero hero);
+        Task UpdateAsync(Hero hero);
+        Task DeleteAsync(Guid id);
+        // ... тук ще добавяме още по-специфични методи по-късно
+    }
+}
